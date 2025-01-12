9d298058439d9b4a2614dbdab94807874fc2dba5
halawardeh <halawardeh12102001@gmail.com>
2025-01-12 11:52:04 +0300

    add more sections to home page

---
+++ Halal/Home.js
@@ -0,0 +1,16 @@
+function bigImg(element) {
+    element.style.transform = "scale(1.5)";
+  }
+  
+  function normalImg(element) {
+    element.style.transform = "scale(1)";
+  }
+  
+
+  function setBackgroundImg(element){
+
+  }
+
+  function removeBackgroundImg(element){
+    
+  }
